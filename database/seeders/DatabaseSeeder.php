de24e97cbf3e23dccf4e1af04d41d0d30f546e8d
shahriar201 <shahriarbrine201@gmail.com>
2021-09-01 23:10:35 +0600

    Category added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name = 'Shahriar Islam';
+        $user->email = 'shahriar@gmail.com';
+        $user->email_verified_at = now();
+        $user->password = Hash::make('12345678');
+        $user->remember_token = Str::random(10);
+        $user->save();
     }
 }
